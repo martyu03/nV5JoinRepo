c8ada95625fd2c5e10b1299ffd03f626fc000ce6
brian <=>
2025-01-26 19:21:53 +0800

    changes

--- src/app/sections/Home/view/HomeStoriesView.tsx
+++ src/app/sections/Home/view/HomeStoriesView.tsx
@@ -4,25 +4,30 @@ import Home from "@/app/page";
 import Link from "next/link";
 
 const HomeStoriesView = () => {
-  return <div>
-    <h1>Top Stories</h1>
-    {topstoriesLinks?.map((item:any, index:number)=>(
-      <HomeStoriesItems key={index} item={item} />
-    ))}
-  </div>;
+  return (
+    <div>
+      <h1>Top Stories</h1>
+      <div className="">
+        {topstoriesLinks?.map((item: any, index: number) => (
+          <HomeStoriesItems key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-const HomeStoriesItems = ({item}) => {
-
-  return(
-    <Link href={item.path} target="_blank">
-      <h3 className="font-semibold">{item?.title}</h3>
-      <div className="flex items-center">
-        <p>{item?.datePublishedOn}</p>
-        <p>{item?.timePublished}</p>
-      </div>
-    </Link>
-  )
-}
+const HomeStoriesItems = ({ item }) => {
+  return (
+    <div className="p-3 border-2 border-t-slate-50">
+      <Link href={item.path} target="_blank">
+        <h3 className="font-semibold">{item?.title}</h3>
+        <div className="flex items-center">
+          <p>{item?.datePublishedOn}</p>
+          <p>{item?.timePublished}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default HomeStoriesView;
